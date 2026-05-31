698ea67b1f2617582d0c26bcf480a6dc5cf2c310
Sura Itana <suraitana@gmail.com>
2026-05-31 17:47:03 +0300

    migrate to async functions

--- app/routers/health.py
+++ app/routers/health.py
@@ -8,5 +8,5 @@ health_service = HealthService()
 
 
 @router.get("/health", response_model=HealthResponse)
-def health_check() -> HealthResponse:
+async def health_check() -> HealthResponse:
     return HealthResponse(**health_service.get_system_health())
